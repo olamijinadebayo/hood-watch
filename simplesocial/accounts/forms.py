a61b66dd038dfa1905c62265dc9d9a4433063a40
olamijinadebayo <consultolamijin@gmail.com>
2018-05-10 06:25:29 +0300

    Create User Models and Signup form
    This will add a User model,create a form class for users to signup and then link
    it in the views

--- simplesocial/accounts/forms.py
+++ simplesocial/accounts/forms.py
@@ -7,3 +7,9 @@ class UserCreateForm(UserCreationForm):
     class Meta:
         fields = ('username','email','password1','password2')
         model = get_user_model()
+
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        self.fields['username'].label = 'Display Name'
+        self.fields['email'].label = "Email Address"
+        
