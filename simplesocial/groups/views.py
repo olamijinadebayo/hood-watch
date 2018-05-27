9b778b972469561a0042608fb89ade6e49a22a45
olamijinadebayo <consultolamijin@gmail.com>
2018-05-27 13:33:04 +0300

    This will add leave and join routes in the view function of the groups app

--- simplesocial/groups/views.py
+++ simplesocial/groups/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import(
     LoginRequiredMixin,
     PermissionRequiredMixin
 )
+from django.contrib import messages
 from django.core.urlresolvers import reverse
+from django.db import IntegrityError
 from django.views import generic
 from groups.models import Group, GroupMember
+from . import models
 # Create your views here.
 
 
