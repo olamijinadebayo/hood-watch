ca3ca5b2b5bfd531e01768786f0d5923c0a13749
olamijinadebayo <consultolamijin@gmail.com>
2018-05-27 09:54:13 +0300

    This wil add a class view in the group app to enable users leave a particular group

--- groups/views.py
+++ groups/views.py
@@ -5,15 +5,39 @@ from django.contrib.auth.mixins import(
 )
 from django.core.urlresolvers import reverse
 from django.views import generic
-from groups.models import Group,GroupMember
+from groups.models import Group, GroupMember
 # Create your views here.
+
 
 class CreateGroup(LoginRequiredMixin, generic.CreateView):
     fields = ("name", "description")
     model = Group
 
+
 class SingleGroup(generic.DetailView):
     model = Group
 
-# class ListGroups(generic.ListView):
-#     model = Group
+
+class ListGroups(generic.ListView):
+    model = Group
+
+
+class JoinGroup(LoginRequiredMixin, generic.RedirectView):
+
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse("groups:single", kwargs={"slug": self.kwargs.get("slug")})
+
+    def get(self, request, *args, **kwargs):
+        group = get_object_or_404(Group, slug=self.kwargs.get("slug"))
+
+        try:
+            GroupMember.objects.create(user=self.request.user, group=group)
+
+        except IntegrityError:
+            messages.warning(self.request, ("Warning, already a member of {}".format(group.name)))
+
+        else:
+            messages.success(
+                self.request, "You are now a member of the {} group.".format(group.name))
+
+        return super().get(request, *args, **kwargs)
