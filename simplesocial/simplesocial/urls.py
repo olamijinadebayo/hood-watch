bb4c64ade156d9304514cc482aafd8464923ae33
olamijinadebayo <consultolamijin@gmail.com>
2018-05-27 10:45:34 +0300

    This will add the url routes for creating post and groups in the project navbar

--- simplesocial/simplesocial/urls.py
+++ simplesocial/simplesocial/urls.py
@@ -15,14 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 
-from django.conf.urls import url,include
+from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.HomePage.as_view(),name='home'),
-    url(r'^accounts/',include('accounts.urls', namespace ='accounts')),
-    url(r'^accounts/',include('django.contrib.auth.urls')),
-    url(r'^test/$',views.TestPage.as_view(),name ='test'),
-    url(r'^thanks/$',views.ThanksPage.as_view(),name ='thanks')
+    url(r'^$', views.HomePage.as_view(), name='home'),
+    url(r'^accounts/', include('accounts.urls', namespace='accounts')),
+    url(r'^accounts/', include('django.contrib.auth.urls')),
+    url(r'^test/$', views.TestPage.as_view(), name='test'),
+    url(r'^thanks/$', views.ThanksPage.as_view(), name='thanks'),
+    url(r"^posts/", include("posts.urls", namespace="posts")),
+    url(r"^groups/", include("groups.urls", namespace="groups")),
 ]
