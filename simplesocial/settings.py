bac71690b67baf7b50893d9ea2df032aa3d558a8
olamijinadebayo <consultolamijin@gmail.com>
2018-05-29 11:48:16 +0300

    this will change where the login redirect url yo the home page

--- simplesocial/settings.py
+++ simplesocial/settings.py
@@ -132,5 +132,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-LOGIN_REDIRECT_URL = 'test'
+LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = 'thanks'
